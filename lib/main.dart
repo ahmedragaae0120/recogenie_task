f964277dcb785c84a4c2306eb631b9a5ce4a5676
اسمك <ahmedbasha01208@gmail.com>
2025-07-24 23:32:24 +0300

    feat(firebase): integrate Firebase core and configuration for the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:recogenie_task/firebase_options.dart';
 import 'package:recogenie_task/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recogenie_task/config/app_theme.dart';
 import 'package:recogenie_task/core/utils/app_routes.dart';
-import 'package:recogenie_task/ui/auth/signup/signup_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Restaurant App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.signup,
+      initialRoute: AppRoutes.login,
       routes: AppRoutes.routes,
     );
   }
